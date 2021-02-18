f59ccff3eb47ce515134edaa578a6e6bc465311a
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-18 13:15:08 +0100

    models validation and db seed completed. controllers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+require 'faker'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+puts "Creating restaurants..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+10.times do
+  ingredient = Ingredient.create!(name: Faker::Food.ingredient )
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
